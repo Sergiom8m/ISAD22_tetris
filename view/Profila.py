0fb7be70c6f458ca92c4cb1fb6a225d51cea9fb4
Sergiomm88 <smartin128@ikasle.ehu.eus>
2022-10-03 12:36:35 +0200

    Pantaila berriak sortu

--- view/Profila.py
+++ view/Profila.py
@@ -1,20 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
-class HasierakoMenua(object):
+class Profila(object):
     """docstring for JokatuLeioa"""
 
     def __init__(self):
-        super(HasierakoMenua, self).__init__()
+        super(Profila, self).__init__()
         self.window = tk.Tk()
         self.window.geometry('400x400')
-        self.window.title("Hasierako Menua")
+        self.window.title("Jokalariaren Profila")
 
         separador = ttk.Label(self.window, text='       ', font=("Calibri", 25))
 
         separador.pack()
 
-        izenburua = ttk.Label(self.window, text='TETRIS', font=("Calibri", 25))
+        izenburua = ttk.Label(self.window, text='ZURE PROFILA', font=("Calibri", 25))
 
         izenburua.pack()
 
@@ -29,14 +29,14 @@ class HasierakoMenua(object):
 
         separador.pack()
 
-        button = tk.Button(self.window, text="Identifikatu", width=30)
+        button = tk.Button(self.window, text="Zure pasahitza aldatu", width=30)
         button.pack(ipadx=10, ipady=10)
 
         separador = ttk.Label(self.window, text='       ', font=("Calibri", 2))
 
         separador.pack()
 
-        button = tk.Button(self.window, text="Erregistratu", width=30)
+        button = tk.Button(self.window, text="Erabiltzaileak ezabatu", width=30)
         button.pack(ipadx=10, ipady=10)
 
         separador = ttk.Label(self.window, text='       ', font=("Calibri", 25))
