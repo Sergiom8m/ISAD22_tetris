e2a206c6c59ebd6514a124b4aa48c663babe52d9
Maits27 <maitiurlo@gmail.com>
2022-10-09 00:01:14 +0200

    datubase intento

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from view.JokatuLehioa import JokatuLehioa
+import sqlite3
 
 
 if __name__ == '__main__':
+	con = sqlite3.connect("datubase.db")
+	cur = con.cursor()
+	#cur.execute("CREATE TABLE JOKALARIAK(erabiltzailea, pasahitza, puntuazioa)")
+	cur.execute("INSERT INTO JOKALARIAK VALUES ('admin',12345, 0 )")
+	for row in cur.execute("SELECT erabiltzailea, puntuazioa FROM JOKALARIAK"):
+		print(row)
+
 	tetris = JokatuLehioa()
