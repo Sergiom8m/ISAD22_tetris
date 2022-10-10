d62db64bd4d4ae54a66d2c5d6ae8b4f9a20f6c47
Sergiomm88 <smartin128@ikasle.ehu.eus>
2022-10-10 12:20:02 +0200

    Def init jarri

--- view/Ezarpenak.py
+++ view/Ezarpenak.py
@@ -1,54 +1,57 @@
 import tkinter as tk
 from tkinter import ttk
+
 class Ezarpenak():
 
-    ezarpenak = tk.Tk()
-    ezarpenak.geometry('400x400')
-    ezarpenak.title("Tetris jokoa")
-    window.resizable(False, False)
-    #ezarpenak.iconbitmap('/home/aingeru/PycharmProjects/ISAD22_tetris/Irudiak')
-    espacio = ttk.Label(ezarpenak, text="")
-    titulo = tk.Label(ezarpenak, text="Partidaren ezarpenak", font=("Times New Roman",25))
-    titulo.pack()
-    espacio.pack()
+    def __init__(self):
+        super(Ezarpenak, self).__init__()
+        self.window = tk.Tk()
+        self.window.geometry('400x400')
+        self.window.title("Tetris jokoa")
+        self.window.resizable(False, False)
 
-    mensaje = tk.Label(ezarpenak, text="Partidaren ezaugarriak aukera itzazu", font=("Calibri"))
-    mensaje.pack()
+        espacio = ttk.Label(self.window, text="")
+        titulo = tk.Label(self.window, text="Partidaren ezarpenak", font=("Times New Roman",25))
+        titulo.pack()
+        espacio.pack()
 
-    espacio.pack()
+        mensaje = tk.Label(self.window, text="Partidaren ezaugarriak aukera itzazu", font=("Calibri"))
+        mensaje.pack()
 
-    titulo_abiadura = tk.Label(ezarpenak, text="Abiadura:", font=("Calibri", 14))
-    titulo_abiadura.place(x=60, y= 100)
+        espacio.pack()
 
-    opcion = tk.IntVar()
+        titulo_abiadura = tk.Label(self.window, text="Abiadura:", font=("Calibri", 14))
+        titulo_abiadura.place(x=60, y= 100)
 
-    #Meter Frame: TODO
-
-    tk.Radiobutton(ezarpenak, text="Erraza", variable=opcion,value=1).place(x=50, y=140)
-    tk.Radiobutton(ezarpenak, text="Ertaina", variable=opcion,value=2).place(x=130, y=140)
-    tk.Radiobutton(ezarpenak, text="Zaila", variable=opcion,value=3).place(x=210, y=140)
-    tk.Radiobutton(ezarpenak, text="Oso zaila", variable=opcion,value=4).place(x=280, y=140)
+        opcion = tk.IntVar()
 
 
 
-    titulo_tamaina = tk.Label(ezarpenak, text="Tamaina:", font=("Calibri", 14))
-    titulo_tamaina.place(x=60, y=180)
-
-    opcion2 = tk.IntVar()
-
-
-    tk.Radiobutton(ezarpenak, text="Erraza", variable=opcion2, value=1).place(x=50, y=220)
-    tk.Radiobutton(ezarpenak, text="Ertaina", variable=opcion2, value=2).place(x=130, y=220)
-    tk.Radiobutton(ezarpenak, text="Zaila", variable=opcion2, value=3).place(x=210, y=220)
-    tk.Radiobutton(ezarpenak, text="Oso zaila", variable=opcion2, value=4).place(x=280, y=220)
-
-    buttonirten = tk.Button(ezarpenak, text="Irten", width=8, font=("Calibri"))
-    buttonirten.place(x=60, y=300)
-    buttonj = tk.Button(ezarpenak, text="Erregistratu", width=8, font=("Calibri"))
-    buttonj.place(x=220, y=300)
+        tk.Radiobutton(self.window, text="Erraza", variable=opcion,value=1).place(x=50, y=140)
+        tk.Radiobutton(self.window, text="Ertaina", variable=opcion,value=2).place(x=130, y=140)
+        tk.Radiobutton(self.window, text="Zaila", variable=opcion,value=3).place(x=210, y=140)
+        tk.Radiobutton(self.window, text="Oso zaila", variable=opcion,value=4).place(x=280, y=140)
 
 
-    ezarpenak.mainloop()
+
+        titulo_tamaina = tk.Label(self.window, text="Tamaina:", font=("Calibri", 14))
+        titulo_tamaina.place(x=60, y=180)
+
+        opcion2 = tk.IntVar()
+
+
+        tk.Radiobutton(self.window, text="Erraza", variable=opcion2, value=1).place(x=50, y=220)
+        tk.Radiobutton(self.window, text="Ertaina", variable=opcion2, value=2).place(x=130, y=220)
+        tk.Radiobutton(self.window, text="Zaila", variable=opcion2, value=3).place(x=210, y=220)
+        tk.Radiobutton(self.window, text="Oso zaila", variable=opcion2, value=4).place(x=280, y=220)
+
+        buttonirten = tk.Button(self.window, text="Irten", width=8, font=("Calibri"))
+        buttonirten.place(x=60, y=300)
+        buttonj = tk.Button(self.window, text="Erregistratu", width=8, font=("Calibri"))
+        buttonj.place(x=220, y=300)
+
+
+        self.window.mainloop()
 
 
 
--- view/HasierakoMenua.py
+++ view/HasierakoMenua.py
@@ -1,48 +1,54 @@
 import tkinter as tk
+from _curses import window
 from tkinter import ttk
+
+from view.Ezarpenak import Ezarpenak
+
 
 class HasierakoMenua(object):
 
-    window = tk.Tk()
-    window.geometry('400x400')
-    window.title("Hasierako Menua")
-    window.resizable(False, False)
-
-    separador = ttk.Label(window, text='       ', font=("Calibri", 25))
-
-    separador.pack()
-
-    izenburua = ttk.Label(window, text='TETRIS', font=("Calibri", 25))
-
-    izenburua.pack()
-
-    separador = ttk.Label(window, text='       ', font=("Calibri", 25))
-
-    separador.pack()
-
-    button = tk.Button(window, text="Partida berria hasi", width=30)
-    button.pack(ipadx=10, ipady=10)
-
-    separador = ttk.Label(window, text='       ', font=("Calibri", 2))
-
-    separador.pack()
-
-    button = tk.Button(window, text="Identifikatu", width=30)
-    button.pack(ipadx=10, ipady=10)
-
-    separador = ttk.Label(window, text='       ', font=("Calibri", 2))
-
-    separador.pack()
-
-    button = tk.Button(window, text="Erregistratu", width=30)
-    button.pack(ipadx=10, ipady=10)
-
-    separador = ttk.Label(window, text='       ', font=("Calibri", 25))
-
-    separador.pack()
-
-    button = tk.Button(window, text="Irten")
-    button.pack(ipadx=10, ipady=10)
+    def __init__(self):
+        super(HasierakoMenua, self).__init__()
+        self.window = tk.Tk()
+        self.window.geometry('400x400')
+        self.window.title("Hasierako Menua")
+        self.window.resizable(False, False)
 
 
-    window.mainloop()
+        separador = ttk.Label(window, text='       ', font=("Calibri", 25))
+
+        separador.pack()
+
+        izenburua = ttk.Label(window, text='TETRIS', font=("Calibri", 25))
+
+        izenburua.pack()
+
+        separador = ttk.Label(window, text='       ', font=("Calibri", 25))
+
+        separador.pack()
+
+        buttonPartidaBerria = tk.Button(window, text="Partida berria hasi", width=30)
+        buttonPartidaBerria.pack(ipadx=10, ipady=10)
+
+        separador = ttk.Label(window, text='       ', font=("Calibri", 2))
+
+        separador.pack()
+
+        buttonIdentifikatu = tk.Button(window, text="Identifikatu", width=30)
+        buttonIdentifikatu.pack(ipadx=10, ipady=10)
+
+        separador = ttk.Label(window, text='       ', font=("Calibri", 2))
+
+        separador.pack()
+
+        buttonErregistratu = tk.Button(window, text="Erregistratu", width=30)
+        buttonErregistratu.pack(ipadx=10, ipady=10)
+
+        separador = ttk.Label(window, text='       ', font=("Calibri", 25))
+
+        separador.pack()
+
+        buttonIrten = tk.Button(window, text="Irten")
+        buttonIrten.pack(ipadx=10, ipady=10)
+
+        window.mainloop()
