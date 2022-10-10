8363aae7f8e50d78c07f8d801e87e17b889f7193
MaiMai <maidertatoherrera@gmail.com>
2022-10-10 11:17:16 +0200

    no hagais caso a esto

--- view/erregistroa.py
+++ view/erregistroa.py
@@ -2,30 +2,64 @@
 
 import tkinter as tk
 from tkinter import ttk
+
+# Koloreak
+botoi_kolor = "#fdb4bf"
+sarrera_kolor = "#f99aaa"
+
+
 class Erregistroa(object):
 
-    #super(HasierakoMenua, self) init()
     window = tk.Tk()
-    window.config(width=780, height=780)
+    window.geometry('780x780')
     window.title("ERREGISTROA")
 
-    izena = tk.Label(window, text="ERREGISTROA")
-
+    # IZENBURUA
+    izena = ttk.Label(window, text="ERREGISTROA", font=("Times New Roman", 25))
     izena.pack()
 
-    
+    tab = ttk.Label(window, text="  ")
+    tab.pack()
+
+    # AZPIZENBURUA
+    testua = ttk.Label(window, text="Mesedez, zeure burua erregistratu", font = ("Times New Roman", 16))
+    testua.pack()
+    tab.pack()
+
+    # BOTOIAK
+    sartu = tk.Button(window, text="Sartu", cursor="hand2", bg=botoi_kolor, width=10, font=("Times New Roman", 14))
+    sartu.place(x=410, y=405)
+
+    irten = tk.Button(window, text="Irten", cursor="hand2", bg=botoi_kolor, width=10, font=("Times New Roman", 14))
+    irten.place(x=230, y=405)
+
+    #EGUNERAPENAK
+    identifik = tk.StringVar()
+    pasahitza1 = tk.StringVar()
+    pasahitza2 = tk.StringVar()
 
     # Identifikatzailearen sarrera
-    identifik = ttk.Entry()
-    identifik.place(x=340, y=50)
+    erab = ttk.Label(window, text='Identifikatzailea... ', font=("Times New Roman", 16))
+    erab.place(x=220, y=120)
 
+    erabil = tk.Entry(window, textvar=identifik, width=30, bg=sarrera_kolor, font=("Times New Roman", 16))
+    erabil.place(x=210, y=150)
     identifik = ttk.Entry(justify=tk.LEFT)
 
     # Pasahitzaren sarrera
-    pasahitz = ttk.Entry()
-    pasahitz.place(x=340, y=150)
+    pas1 = ttk.Label(window, text='Pasahitza... ', font=("Times New Roman", 16))
+    pas1.place(x=220, y=220)
 
-    pasahitz = ttk.Entry(show="*")
+    pasahitz1 = tk.Entry(window, textvar=pasahitza1, width=30, bg=sarrera_kolor, font=("Times New Roman", 16))
+    pasahitz1.place(x=210, y=250)
+    pasahitz1 = ttk.Entry(justify=tk.LEFT)
+
+    pas2 = ttk.Label(window, text='Pasahitza errepikatu... ', font=("Times New Roman", 16))
+    pas2.place(x=220, y=320)
+
+    pasahitz2 = tk.Entry(window, textvar=pasahitza2, width=30, bg=sarrera_kolor, font=("Times New Roman", 16))
+    pasahitz2.place(x=210, y=350)
+    pasahitz2 = ttk.Entry(justify=tk.LEFT)
 
 
     window.mainloop()
